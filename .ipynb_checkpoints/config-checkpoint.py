5b5116404f48b05ee3756187c8b782eb4cb82cdb
jonathancosme <i.jonathan.cosme@gmail.com>
2021-09-27 11:40:31 -0500

    created a v2 version of the .ipynb that checks for image files, does not process images that have already been processed, and also created an output file of results

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,15 @@
+folder_locs = {'in_pics': './input_photos/',
+               'out_pics': './output_photos/',
+               'in_files': './input_csvs/',
+              }
+
+interest_cols = {'x1': '4-7: Img-bbox(TL_x',
+              'y1': 'TL_y',
+              'x2': 'BR_x',
+              'y2': 'BR_y)',
+              'label': '10-11+: Repeated Species',
+              'filename': 'File Name',
+             }
+
+rando_seed = 42
+
